115562e18b619f6b9a7ba49ba264e878324f4a33
eried132 <eried132@student.liu.se>
2019-09-17 19:52:46 +0200

    20190915.1

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero} from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'Derpman'},
+  {id: 12, name: 'Haschlasse'},
+  {id: 13, name: 'Dr.fiskhatt'},
+  {id: 14, name: 'Majorblazor'},
+  {id: 15, name: 'Kingen'},
+  {id: 16, name: 'Knugen'},
+  {id: 17, name: 'Häffaklumpen'},
+  {id: 18, name: 'YEAAAHH'},
+  {id: 19, name: 'Meh'},
+  {id: 20, name: 'NIL'},
+];
